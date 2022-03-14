4db45ddda80ae3d10586a5fa580e8e9eda3b4929
ericksih <erick.saputra08@gmail.com>
2022-03-15 05:30:41 +0700

    materialize Init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 function App() {
+  useEffect(() => {
+    M.AutoInit();
+  }, []);
+
   return <div className='App'>hello</div>;
 }
 
